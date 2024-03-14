b8400243f66d65cf041b6da091aac4832c12b650
aarinz <aarinzacharia560@gmail.com>
2024-03-14 11:10:04 +0530

    fixed static files error

--- ecom/settings.py
+++ ecom/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+
+STATIC_URL = '/ecom/static/' 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    
+    os.path.join(BASE_DIR, 'ecom', 'static')  
 ]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
