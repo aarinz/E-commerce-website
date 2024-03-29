2719458261abed6006ab6ede8cb9e97f2d091cde
aarinz <aarinzacharia560@gmail.com>
2024-03-29 07:48:49 +0530

    cart's total data added

--- ecom_app/views.py
+++ ecom_app/views.py
@@ -14,9 +14,10 @@ def  cart(request):
         items = order.orderitem_set.all()
     else:
         items=[]
+        order = {'get_cart_total':0, 'get_cart_items':0} #for loggedout user
 
 
-    context = {'items':items}
+    context = {'items':items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
